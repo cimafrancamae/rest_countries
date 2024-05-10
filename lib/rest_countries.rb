f168a0cff29ccc0744f9eb4dec7e7732d36068e4
maeci <cimafrancamae@gmail.com>
2024-05-11 00:48:28 +0800

    refactor: remove autoloads

--- lib/rest_countries.rb
+++ lib/rest_countries.rb
@@ -1,8 +1,5 @@
 # frozen_string_literal: true
 
 require_relative "rest_countries/version"
-
-module RestCountries
-  autoload :Client, "rest_countries/client"
-  autoload :Error, "rest_countries/error"
-end
+require_relative "rest_countries/client"
+require_relative "rest_countries/error"
